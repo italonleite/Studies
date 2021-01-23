bd9f482fdca5d8e41b12aaf62dd2e64bd15aa009
italo.leite <italo.leite@nibo.com.br>
2021-01-23 20:47:47 -0300

    adicionando dbContext

---
+++ Entity Framework RelationShip/Data/ImmobileDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Entity_Framework_RelationShip.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity_Framework_RelationShip.Data
+{
+    public class ImmobileDbContext : DbContext
+    {
+        public DbSet<Immobile> Immobiles { get; set; }
+        public DbSet<Address> Adresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DEV_ITALOLEITE\SQLEXPRESS;Database=EFRelationShip;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        }
+    }
+}
