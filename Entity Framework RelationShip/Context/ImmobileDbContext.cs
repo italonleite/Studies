6364b8fe1709608f2ed2511fd5f2c8335e49c802
italo.leite <italo.leite@nibo.com.br>
2021-01-26 00:53:11 -0300

    inserindo dbContext

--- Entity Framework RelationShip/Context/ImmobileDbContext.cs
+++ Entity Framework RelationShip/Context/ImmobileDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Entity_Framework_RelationShip.Models;
+﻿using Entity_Framework_RelationShip.Mapping;
+using Entity_Framework_RelationShip.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,12 +10,18 @@ namespace Entity_Framework_RelationShip.Data
     public class ImmobileDbContext : DbContext
     {
         public DbSet<Immobile> Immobiles { get; set; }
-        public DbSet<Address> Adresses { get; set; }
+        public DbSet<Address> Addresses { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=DEV_ITALOLEITE\SQLEXPRESS;Database=EFRelationShip;Trusted_Connection=True;MultipleActiveResultSets=true");
+           // optionsBuilder.LogTo(Console.WriteLine);
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ImmobileMap());
+            modelBuilder.ApplyConfiguration(new AddressMap());
         }
     }
 }
