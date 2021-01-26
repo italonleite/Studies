c9786a46e0d3c6fb1be820675347f4600f114caf
italo.leite <italo.leite@nibo.com.br>
2021-01-26 00:53:51 -0300

    mapeando entidade com fluent api

---
+++ Entity Framework RelationShip/Mapping/AddressMap.cs
@@ -0,0 +1,23 @@
+﻿using Entity_Framework_RelationShip.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity_Framework_RelationShip.Mapping
+{
+    public class AddressMap : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.ToTable("Addresses");
+            builder.HasKey(a => a.AddressId);
+            builder.Property(a => a.ZipCode).HasColumnType("varchar(9)");
+            builder.Property(a => a.Neighborhood).HasColumnType("varchar(200)");
+            builder.Property(a => a.StreetAddress).HasColumnType("varchar(100)");
+            builder.Property(a => a.City).HasColumnType("varchar(50)");
+            builder.Property(a => a.State).HasColumnType("varchar(2)");
+        }
+    }
+}
---
+++ Entity Framework RelationShip/Mapping/ImmobileMap.cs
@@ -0,0 +1,23 @@
+﻿using Entity_Framework_RelationShip.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity_Framework_RelationShip.Mapping
+{
+    class ImmobileMap : IEntityTypeConfiguration<Immobile>
+    {
+        public void Configure(EntityTypeBuilder<Immobile> builder)
+        {
+            builder.ToTable("Imobiles");
+            builder.HasKey(i => i.ImmobileId);
+            builder.Property(i => i.EType);
+            builder.HasOne(i => i.Address)
+                   .WithOne()
+                   .HasForeignKey<Immobile>(a => a.AddressId);
+        }
+
+    }
+}
