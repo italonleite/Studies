49d53c20ad104fdab38b37b7b382fe9b71437324
italo.leite <italo.leite@nibo.com.br>
2021-02-08 12:54:54 -0300

    adicionando as entiidades

---
+++ DescriminatorTest/Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DescriminatorTest.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+    }
+    
+}
---
+++ DescriminatorTest/Models/RssBlog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DescriminatorTest.Models
+{
+    public class RssBlog : Blog
+    {
+        public string RssUrl { get; set; }
+    }
+}
