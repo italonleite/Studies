0a8040d5767c65f72d05d15a4208f6820d7c14f9
italo.leite <italo.leite@nibo.com.br>
2021-02-08 12:54:18 -0300

    adicionando projeto para mapeamento de tabela por descriminator

---
+++ DescriminatorTest/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using DescriminatorTest.Context;
+using DescriminatorTest.Models;
+using System;
+
+namespace DescriminatorTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new DiscriminatorTet())
+            {
+                var blog = new Blog()
+                {
+                    BlogId = 1,
+                    Url = "www.xyz.com.br",
+                };
+
+                var rssBlog = new RssBlog()
+                {
+                             
+                   RssUrl = "www.rss.com.br",
+                };
+
+                db.Add(blog);
+                db.Add(rssBlog);
+                db.SaveChanges();
+                   
+                
+
+
+            }
+
+        }
+    }
+}
