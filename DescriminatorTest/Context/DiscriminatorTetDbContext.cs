d17aa6fa50fff4c3e0d3b69cd48dc8634c01de9d
italo.leite <italo.leite@nibo.com.br>
2021-02-08 12:55:05 -0300

    adicionando o contexto

---
+++ DescriminatorTest/Context/DiscriminatorTetDbContext.cs
@@ -0,0 +1,38 @@
+﻿using DescriminatorTest.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DescriminatorTest.Context
+{
+    public class DiscriminatorTet : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<RssBlog> RssBlogs { get; set; }
+        //  public DbSet<Person> People { get; set; }
+
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DEV_ITALOLEITE\SQLEXPRESS;Database=EFRelatDiscriminator;Trusted_Connection=True;MultipleActiveResultSets=true");
+            // optionsBuilder.LogTo(Console.WriteLine);
+        }
+
+        protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().HasKey(a => a.BlogId);
+             modelBuilder.Entity<Blog>()
+            .HasDiscriminator<string>("blog_type")
+            .HasValue<Blog>("blog_base")
+            .HasValue<RssBlog>("blog_rss");
+           
+
+
+        }
+    }
+
+   
+}
