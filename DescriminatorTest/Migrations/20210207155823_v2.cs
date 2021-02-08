d0a81963af6c516f8d5ae540e5809638ba2133b6
italo.leite <italo.leite@nibo.com.br>
2021-02-08 12:55:36 -0300

    add migrationts

---
+++ DescriminatorTest/Migrations/20210207155823_v2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DescriminatorTest.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "blog_type",
+                table: "Blogs",
+                newName: "Discriminator");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Discriminator",
+                table: "Blogs",
+                newName: "blog_type");
+        }
+    }
+}
